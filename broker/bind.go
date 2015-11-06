94220bfdae21764f7ebe406928f549e0568a0a6a
Dr Nic Williams <drnicwilliams@gmail.com>
2015-11-05 20:47:41 -0800

    Bind() looks up published port

--- broker/bind.go
+++ broker/bind.go
@@ -1,13 +1,31 @@
 package broker
 
-import "github.com/frodenas/brokerapi"
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/cloudfoundry-community/patroni-broker/serviceinstance"
+	"github.com/frodenas/brokerapi"
+)
 
 // Bind returns access credentials for a service instance
-func (broker *Broker) Bind(instanceID string, bindingID string, details brokerapi.BindDetails) (brokerapi.BindingResponse, error) {
+func (bkr *Broker) Bind(instanceID string, bindingID string, details brokerapi.BindDetails) (brokerapi.BindingResponse, error) {
+	cluster := serviceinstance.NewCluster(instanceID, brokerapi.ProvisionDetails{}, bkr.EtcdClient, bkr.Logger)
+
+	key := fmt.Sprintf("/routing/allocation/%s", cluster.InstanceID)
+	resp, err := cluster.EtcdClient.Get(key, false, false)
+	if err != nil {
+		return brokerapi.BindingResponse{}, fmt.Errorf("Internal error: no published port for provisioned cluster")
+	}
+	publicPort, err := strconv.ParseInt(resp.Node.Value, 10, 64)
+	if err != nil {
+		return brokerapi.BindingResponse{}, fmt.Errorf("Internal error: published port is not an integer (%s)", resp.Node.Value)
+	}
+
 	return brokerapi.BindingResponse{
 		Credentials: brokerapi.CredentialsHash{
 			Host: "10.10.10.10",
-			Port: 40000,
+			Port: publicPort,
 		},
 	}, nil
 }
