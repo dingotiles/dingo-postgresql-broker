bfa191de86b7758fa1642c02bc8f94cd79318105
Dr Nic Williams <drnicwilliams@gmail.com>
2015-11-14 16:27:32 -0800

    cli commands can take $PATRONI_BROKER_CONFIG instead of --config

--- clicmd/show_cells.go
+++ clicmd/show_cells.go
@@ -3,19 +3,27 @@ package clicmd
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/cloudfoundry-community/patroni-broker/backend"
 	"github.com/cloudfoundry-community/patroni-broker/config"
 	"github.com/codegangsta/cli"
 )
 
-// ShowCells displays the status of each backend cell/server
-func ShowCells(c *cli.Context) {
-	configPath := c.String("config")
+func loadConfig(configPath string) (cfg *config.Config) {
+	if os.Getenv("PATRONI_BROKER_CONFIG") != "" {
+		configPath = os.Getenv("PATRONI_BROKER_CONFIG")
+	}
 	cfg, err := config.LoadConfig(configPath)
 	if err != nil {
 		log.Fatal(err)
 	}
+	return
+}
+
+// ShowCells displays the status of each backend cell/server
+func ShowCells(c *cli.Context) {
+	cfg := loadConfig(c.String("config"))
 	etcdClient := backend.NewEtcdClient(cfg.KVStore.Machines, "/")
 	serviceinstances, err := etcdClient.Get("/serviceinstances", true, true)
 	if err != nil {
