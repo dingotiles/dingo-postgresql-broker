6e4a9adf175ea7337f9011a6f7eb2fe4ae729deb
Dr Nic Williams <drnicwilliams@gmail.com>
2016-03-24 10:11:22 +1000

    fix cd

--- ci/scripts/build-and-test.sh
+++ ci/scripts/build-and-test.sh
@@ -5,6 +5,6 @@ set -e -x
 export GOPATH=$PWD/broker:$PWD/broker/src/github.com/dingotiles/patroni-broker/Godeps/_workspace
 export PATH=$GOPATH/bin:$PATH
 
-cd ${GOPATH}/src/github.com/dingotiles/patroni-broker
+cd $PWD/broker/src/github.com/dingotiles/patroni-broker
 go install github.com/onsi/ginkgo/ginkgo
 ginkgo -r "$@"
