1b84702075dece9936fe23b7f097f033381b699d
Dr Nic Williams <drnicwilliams@gmail.com>
2016-04-09 20:23:31 -0500

    don't display ugly golang format of catalog

--- broker/catalog.go
+++ broker/catalog.go
@@ -1,8 +1,6 @@
 package broker
 
 import (
-	"fmt"
-
 	"github.com/frodenas/brokerapi"
 	"github.com/mitchellh/mapstructure"
 )
@@ -10,9 +8,6 @@ import (
 // Services is the catalog of services offered by the broker
 func (bkr *Broker) Services() brokerapi.CatalogResponse {
 	result := &brokerapi.CatalogResponse{}
-	fmt.Printf("%#v\n", bkr.Config.Catalog)
-	fmt.Printf("%#v\n", bkr.Config.Catalog.Services[0].Metadata)
-	fmt.Printf("%#v\n", bkr.Config.Catalog.Services[0].Plans[0].Metadata)
 	err := mapstructure.Decode(&bkr.Config.Catalog, &result)
 	if err != nil {
 		bkr.Logger.Error("services.decode", err)
