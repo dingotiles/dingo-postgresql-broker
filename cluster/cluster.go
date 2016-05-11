ef1478809e8a4c4bef3564d5a419abd9b556db4f
Justin Carter <jcarter@starkandwayne.com>
2016-05-11 14:35:46 +0200

    Move all backend related code into 1 file

--- cluster/cluster.go
+++ cluster/cluster.go
@@ -2,14 +2,10 @@ package cluster
 
 import (
 	"fmt"
-	"math/rand"
-	"regexp"
-	"sort"
 	"time"
 
 	"github.com/dingotiles/dingo-postgresql-broker/backend"
 	"github.com/dingotiles/dingo-postgresql-broker/bkrconfig"
-	"github.com/dingotiles/dingo-postgresql-broker/utils"
 	"github.com/frodenas/brokerapi"
 	"github.com/pivotal-golang/lager"
 )
@@ -110,90 +106,4 @@ func (cluster *Cluster) WaitForRoutingPortAllocation() (err error) {
 	}
 	cluster.logger.Error("provision.routing.timed-out", err, lager.Data{"err": err})
 	return err
-}
-
-// RandomReplicaNode should discover which nodes are replicas and return a random one
-// FIXME - currently just picking a random node - which might be the master
-func (cluster *Cluster) RandomReplicaNode() (nodeUUID string, backend string, err error) {
-	key := fmt.Sprintf("/serviceinstances/%s/nodes", cluster.Data.InstanceID)
-	resp, err := cluster.etcdClient.Get(key, false, true)
-	if err != nil {
-		cluster.logger.Error("random-replica-node.nodes", err)
-		return
-	}
-	item := rand.Intn(len(resp.Node.Nodes))
-	nodeKey := resp.Node.Nodes[item].Key
-	r, _ := regexp.Compile("/nodes/(.*)$")
-	matches := r.FindStringSubmatch(nodeKey)
-	nodeUUID = matches[1]
-
-	key = fmt.Sprintf("/serviceinstances/%s/nodes/%s/backend", cluster.Data.InstanceID, nodeUUID)
-	resp, err = cluster.etcdClient.Get(key, false, false)
-	if err != nil {
-		cluster.logger.Error("random-replica-node.backend", err)
-		return
-	}
-	backend = resp.Node.Value
-
-	return
-}
-
-// AllBackends is a flat list of all Backend APIs
-func (cluster *Cluster) AllBackends() (backends []*bkrconfig.Backend) {
-	return cluster.config.Scheduler.Backends
-}
-
-// AllAZs lists of AZs offered by AllBackends()
-func (cluster *Cluster) AllAZs() (list []string) {
-	azUsage := map[string]int{}
-	for _, backend := range cluster.AllBackends() {
-		azUsage[backend.AvailabilityZone]++
-	}
-	for az := range azUsage {
-		list = append(list, az)
-	}
-	// TEST sorting AZs for benefit of tests
-	sort.Strings(list)
-	return
-}
-
-// if any errors, assume that cluster has no running nodes yet
-func (cluster *Cluster) usedBackendGUIDs() (backendGUIDs []string) {
-	resp, err := cluster.etcdClient.Get(fmt.Sprintf("/serviceinstances/%s/nodes", cluster.Data.InstanceID), false, false)
-	if err != nil {
-		return
-	}
-	for _, clusterNode := range resp.Node.Nodes {
-		nodeKey := clusterNode.Key
-		resp, err = cluster.etcdClient.Get(fmt.Sprintf("%s/backend", nodeKey), false, false)
-		if err != nil {
-			cluster.logger.Error("az-used.backend", err)
-			return
-		}
-		backendGUIDs = append(backendGUIDs, resp.Node.Value)
-	}
-	return
-}
-
-// backendAZsByUnusedness sorts the availability zones in order of whether this cluster is using them or not
-// An AZ that is not being used at all will be early in the result.
-// All known AZs are included in the result
-func (cluster *Cluster) sortBackendAZsByUnusedness() (vs *utils.ValSorter) {
-	backends := cluster.AllBackends()
-	azUsageData := map[string]int{}
-	for _, az := range cluster.AllAZs() {
-		azUsageData[az] = 0
-	}
-	for _, backendGUID := range cluster.usedBackendGUIDs() {
-		for _, backend := range backends {
-			if backend.GUID == backendGUID {
-				azUsageData[backend.AvailabilityZone]++
-			}
-		}
-	}
-	vs = utils.NewValSorter(azUsageData)
-	fmt.Printf("usage %#v\n", azUsageData)
-	vs.Sort()
-	fmt.Printf("sorted %#v\n", vs)
-	return
 }
