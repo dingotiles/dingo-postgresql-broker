0a39ed143f2929c1fb2a9f5db62aac8bf08fb955
Justin Carter <jcarter@starkandwayne.com>
2016-05-22 06:55:40 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-if [[ ! -f tmp/cf.yml ]]; then
-  echo "Place your CF admin credentials into tmp/cf.yml"
-  exit 1
-fi
-export PATRONI_BROKER_CONFIG=tmp/bosh-lite.yml
-spruce merge config/bosh-lite.example.yml tmp/cf.yml > $PATRONI_BROKER_CONFIG
+export PATRONI_BROKER_CONFIG=demo/bosh-lite.yml
 
 go run main.go broker
