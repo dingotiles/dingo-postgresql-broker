9c36d55221d12be3145e870ff9d69b4ccfc82dfb
Dr Nic Williams <drnicwilliams@gmail.com>
2016-07-01 12:31:06 +1000

    additional test for default/non-filtered backends

--- scheduler/scheduler_test.go
+++ scheduler/scheduler_test.go
@@ -33,3 +33,27 @@ func TestScheduler_filterBackendsByCellGUIDs(t *testing.T) {
 		t.Fatalf("Plan should only have one filtered backend")
 	}
 }
+
+func TestScheduler_allBackends(t *testing.T) {
+	t.Parallel()
+
+	testPrefix := "TestScheduler_filterBackendsByCellGUIDs"
+	logger := testutil.NewTestLogger(testPrefix, t)
+
+	config := config.Scheduler{
+		Backends: []*config.Backend{
+			&config.Backend{GUID: "cell-guid1"},
+			&config.Backend{GUID: "cell-guid2"},
+		},
+	}
+	scheduler := NewScheduler(config, logger)
+	features := structs.ClusterFeatures{}
+	plan, err := scheduler.newPlan(nil, features)
+	if err != nil {
+		t.Fatalf("scheduler.newPlan error: %v", err)
+	}
+
+	if len(plan.backends) != 2 {
+		t.Fatalf("Plan should have both backend cells")
+	}
+}
