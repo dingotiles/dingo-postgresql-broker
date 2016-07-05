5a80795394788d931f5768bf07e9faac49ca87da
Dr Nic Williams <drnicwilliams@gmail.com>
2016-07-06 08:52:40 +1000

    response helper; pass {person} to example method

--- broker/adminapi/adminapi.go
+++ broker/adminapi/adminapi.go
@@ -1,6 +1,7 @@
 package adminapi
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
@@ -12,7 +13,7 @@ import (
 func New(serviceBroker brokerapi.ServiceBroker, logger lager.Logger, brokerCredentials brokerapi.BrokerCredentials) http.Handler {
 	router := newHTTPRouter()
 
-	router.Get("/admin/hello/", hello(serviceBroker, router, logger))
+	router.Get("/admin/hello/{person}", hello(serviceBroker, router, logger))
 	return wrapAuth(router, brokerCredentials)
 }
 
@@ -20,11 +21,19 @@ func wrapAuth(router httpRouter, credentials brokerapi.BrokerCredentials) http.H
 	return auth.NewWrapper(credentials.Username, credentials.Password).Wrap(router)
 }
 
+func respond(w http.ResponseWriter, status int, response interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+
+	encoder := json.NewEncoder(w)
+	encoder.Encode(response)
+}
+
 func hello(serviceBroker brokerapi.ServiceBroker, router httpRouter, logger lager.Logger) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
 		vars := router.Vars(req)
-		message := vars["message"]
+		person := vars["person"]
 
-		w.Write([]byte(fmt.Sprintf("admin %s", message)))
+		respond(w, http.StatusOK, fmt.Sprintf("hello, %s", person))
 	}
 }
