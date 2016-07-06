76f04979f7d7ca5e288b7bb80d91d78abe25372b
Dr Nic Williams <drnicwilliams@gmail.com>
2016-07-06 19:30:01 +1000

    Add /admin/service_instances/{instance_id}

--- broker/adminapi.go
+++ broker/adminapi.go
@@ -12,7 +12,8 @@ import (
 func NewAdminAPI(serviceBroker *Broker, logger lager.Logger, brokerCredentials brokerapi.BrokerCredentials) http.Handler {
 	router := newHTTPRouter()
 
-	router.Get("/admin/cells", cells(serviceBroker, router, logger))
+	router.Get("/admin/cells", adminCells(serviceBroker, router, logger))
+	router.Get("/admin/service_instances/{instance_id}", adminServiceInstances(serviceBroker, router, logger))
 	return wrapAuth(router, brokerCredentials)
 }
 
@@ -28,8 +29,29 @@ func respond(w http.ResponseWriter, status int, response interface{}) {
 	encoder.Encode(response)
 }
 
-func cells(serviceBroker *Broker, router httpRouter, logger lager.Logger) http.HandlerFunc {
+func adminCells(bkr *Broker, router httpRouter, logger lager.Logger) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
-		respond(w, http.StatusOK, serviceBroker.Cells())
+		logger := bkr.newLoggingSession("admin.cells", lager.Data{})
+		defer logger.Info("done")
+
+		respond(w, http.StatusOK, bkr.Cells())
+	}
+}
+
+func adminServiceInstances(bkr *Broker, router httpRouter, logger lager.Logger) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		vars := router.Vars(req)
+		instanceID := vars["instance_id"]
+
+		logger := bkr.newLoggingSession("admin.service-instances", lager.Data{"instance-id": instanceID})
+		defer logger.Info("done")
+
+		cluster, err := bkr.state.LoadCluster(instanceID)
+		if err != nil {
+			logger.Error("load-cluster.error", err)
+			respond(w, http.StatusInternalServerError, err.Error())
+		}
+
+		respond(w, http.StatusOK, cluster)
 	}
 }
