97d47e8255ebef38a58843b5ad5de84dca93432d
Dr Nic Williams <drnicwilliams@gmail.com>
2016-07-06 11:46:16 +1000

    plan test - remove node from cluster

--- scheduler/plan_test.go
+++ scheduler/plan_test.go
@@ -66,3 +66,36 @@ func TestPlan_Steps_NewCluster_IncreaseCount(t *testing.T) {
 		t.Fatalf("plan should have steps %v, got %v", expectedStepTypes, stepTypes)
 	}
 }
+
+func TestPlan_Steps_NewCluster_DecreaseCount(t *testing.T) {
+	t.Parallel()
+
+	testPrefix := "TestPlan_Steps_NewCluster_DecreaseCount"
+	logger := testutil.NewTestLogger(testPrefix, t)
+
+	config := config.Scheduler{
+		Backends: []*config.Backend{
+			&config.Backend{GUID: "cell1"},
+			&config.Backend{GUID: "cell2"},
+			&config.Backend{GUID: "cell3"},
+			&config.Backend{GUID: "cell4"},
+		},
+	}
+	scheduler := NewScheduler(config, logger)
+	clusterState := &structs.ClusterState{
+		Nodes: []*structs.Node{
+			&structs.Node{ID: "a", BackendID: "cell1"},
+			&structs.Node{ID: "b", BackendID: "cell2"},
+			&structs.Node{ID: "c", BackendID: "cell3"},
+		},
+	}
+	plan, err := scheduler.newPlan(clusterState, structs.ClusterFeatures{NodeCount: 2})
+	if err != nil {
+		t.Fatalf("scheduler.newPlan error: %v", err)
+	}
+	expectedStepTypes := []string{"RemoveNode"}
+	stepTypes := plan.stepTypes()
+	if !reflect.DeepEqual(stepTypes, expectedStepTypes) {
+		t.Fatalf("plan should have steps %v, got %v", expectedStepTypes, stepTypes)
+	}
+}
