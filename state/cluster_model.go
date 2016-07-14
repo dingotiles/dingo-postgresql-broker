64051b153f1c29e4d68ddda5b2dc97efbf6dd856
Justin Carter <jcarter@starkandwayne.com>
2016-07-14 11:04:45 +0200

    Use rename model -> m in cluster_model.go (shorter lines)

--- state/cluster_model.go
+++ state/cluster_model.go
@@ -19,73 +19,73 @@ func NewClusterModel(state State, cluster structs.ClusterState) *ClusterModel {
 	}
 }
 
-func (model *ClusterModel) save() error {
-	return model.state.SaveCluster(model.cluster)
+func (m *ClusterModel) save() error {
+	return m.state.SaveCluster(m.cluster)
 }
 
 // PlanError stores the failure message for a scheduled Plan
 // This will be shown to end users via /last_operation endpoint
-func (model *ClusterModel) SchedulingError(err error) error {
-	model.cluster.SchedulingInfo.LastMessage = err.Error()
-	model.cluster.SchedulingInfo.Error = true
-	model.cluster.SchedulingInfo.Status = structs.SchedulingStatusFailed
-	return model.save()
+func (m *ClusterModel) SchedulingError(err error) error {
+	m.cluster.SchedulingInfo.LastMessage = err.Error()
+	m.cluster.SchedulingInfo.Error = true
+	m.cluster.SchedulingInfo.Status = structs.SchedulingStatusFailed
+	return m.save()
 }
 
-func (model *ClusterModel) BeginScheduling(steps int) error {
-	model.cluster.SchedulingInfo.Steps = steps
-	model.cluster.SchedulingInfo.CompletedSteps = 0
-	model.cluster.SchedulingInfo.LastMessage = "In Progress..."
-	model.cluster.SchedulingInfo.Error = false
-	model.cluster.SchedulingInfo.Status = structs.SchedulingStatusInProgress
-	return model.save()
+func (m *ClusterModel) BeginScheduling(steps int) error {
+	m.cluster.SchedulingInfo.Steps = steps
+	m.cluster.SchedulingInfo.CompletedSteps = 0
+	m.cluster.SchedulingInfo.LastMessage = "In Progress..."
+	m.cluster.SchedulingInfo.Error = false
+	m.cluster.SchedulingInfo.Status = structs.SchedulingStatusInProgress
+	return m.save()
 }
 
-func (model *ClusterModel) SchedulingStepCompleted() error {
-	model.cluster.SchedulingInfo.CompletedSteps += 1
-	if model.cluster.SchedulingInfo.CompletedSteps == model.cluster.SchedulingInfo.Steps {
-		model.cluster.SchedulingInfo.Status = structs.SchedulingStatusSuccess
-		model.cluster.SchedulingInfo.LastMessage = "Scheduling Completed"
+func (m *ClusterModel) SchedulingStepCompleted() error {
+	m.cluster.SchedulingInfo.CompletedSteps += 1
+	if m.cluster.SchedulingInfo.CompletedSteps == m.cluster.SchedulingInfo.Steps {
+		m.cluster.SchedulingInfo.Status = structs.SchedulingStatusSuccess
+		m.cluster.SchedulingInfo.LastMessage = "Scheduling Completed"
 	}
 
-	return model.save()
+	return m.save()
 }
 
-func (model *ClusterModel) SchedulingStepStarted(stepType string) error {
-	model.cluster.SchedulingInfo.LastMessage = fmt.Sprintf("Perfoming Step: '%s'", stepType)
-	return model.save()
+func (m *ClusterModel) SchedulingStepStarted(stepType string) error {
+	m.cluster.SchedulingInfo.LastMessage = fmt.Sprintf("Perfoming Step: '%s'", stepType)
+	return m.save()
 }
 
-func (model *ClusterModel) SchedulingInfo() structs.SchedulingInfo {
-	return model.cluster.SchedulingInfo
+func (m *ClusterModel) SchedulingInfo() structs.SchedulingInfo {
+	return m.cluster.SchedulingInfo
 }
 
-func (model *ClusterModel) Cluster() structs.ClusterState {
-	return model.cluster
+func (m *ClusterModel) Cluster() structs.ClusterState {
+	return m.cluster
 }
 
-func (model *ClusterModel) InstanceID() structs.ClusterID {
-	return model.cluster.InstanceID
+func (m *ClusterModel) InstanceID() structs.ClusterID {
+	return m.cluster.InstanceID
 }
 
-func (model *ClusterModel) AllocatedPort() int {
-	return model.cluster.AllocatedPort
+func (m *ClusterModel) AllocatedPort() int {
+	return m.cluster.AllocatedPort
 }
 
-func (model *ClusterModel) NodeCount() int {
-	return model.cluster.NodeCount()
+func (m *ClusterModel) NodeCount() int {
+	return m.cluster.NodeCount()
 }
 
-func (model *ClusterModel) Nodes() []*structs.Node {
-	return model.cluster.Nodes
+func (m *ClusterModel) Nodes() []*structs.Node {
+	return m.cluster.Nodes
 }
 
-func (model *ClusterModel) AddNode(node structs.Node) error {
-	model.cluster.AddNode(node)
-	return model.save()
+func (m *ClusterModel) AddNode(node structs.Node) error {
+	m.cluster.AddNode(node)
+	return m.save()
 }
 
-func (model *ClusterModel) RemoveNode(node *structs.Node) error {
-	model.cluster.RemoveNode(node)
-	return model.save()
+func (m *ClusterModel) RemoveNode(node *structs.Node) error {
+	m.cluster.RemoveNode(node)
+	return m.save()
 }
